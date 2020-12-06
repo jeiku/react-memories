c8160ba83a756a2723b940202043162a436b4362
jake <9012257+jeiku@users.noreply.github.com>
2020-12-06 18:04:33 -0500

    refactor code

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import postRoutes from "./routes/posts.js";
 const app = express();
 dotenv.config();
 
-// we will be sending large images, so set limit
+// we will be sending large images, so set limitt
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
